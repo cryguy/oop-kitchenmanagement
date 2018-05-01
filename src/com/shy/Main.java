71367c1c6fbbbdca34727c5f380c187353d0d13d
cryguy <cry@cry.zone>
2018-05-01 10:50:18 +0800

    Add comments and todo's

--- src/com/shy/Main.java
+++ src/com/shy/Main.java
@@ -5,6 +5,22 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+
+        /*
+        ADD MAIN MENU
+         */
+
+        //MAIN MENU
+
+
+        /*
+        ADD FUNCTIONS TO DO AND RETURN TO MAIN MENU
+         */
+
+
+        
+
+        // DEMO OF Ingredient Class
         Ingredient a = new Ingredient("a",50);
         Ingredient b = new Ingredient("b",50);
         Ingredient c = new Ingredient("c",50);
@@ -13,10 +29,20 @@ public class Main {
         listofing.add(a);
         listofing.add(b);
         listofing.add(c);
+
+
+        // DEMO of Product Class
         Product sushi = new Product("sushi",1,50, 5 , listofing);
         ArrayList<Ingredient> produ = sushi.GetIngredients();
         System.out.println(sushi.GetName() + " " + sushi.GetPrice());
         for(Ingredient i : produ)
             System.out.println(i.GetName() + " " + i.GetPrice());
+
+    }
+
+
+
+    private void menu() {
+        //SHOW MENU -- ADD OPTIONS
     }
 }
