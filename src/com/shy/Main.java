bd55b339e8f773baa7ad77d8c3b581c099d9fc75
Denish Nathan <denishbuilder10@gmail.com>
2018-05-01 12:02:48 +0800

    Main Menu
    -Moved Main menu to menu()
    -How to call menu()

--- src/com/shy/Main.java
+++ src/com/shy/Main.java
@@ -7,32 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //denish was here
-        //main menu i guess
-        int main_menu_op=0;
+        // how to call menu() here?
 
-        System.out.print("Main Menu\n\n 1. Order\n 2. Product Management\n 3. Inventory Management\n\nChoose your option: ");
-        Scanner mmo = new Scanner(System.in);
-        main_menu_op = mmo.nextInt();
-        switch(main_menu_op){
-            case 1:
-                System.out.printf("1");
-                //should display products from products class and need to create another switch here
-                //order
-                break;
-            case 2:
-                System.out.printf("2");
-                //product_management
-                break;
-            case 3:
-                System.out.printf("3");
-                //inventory_management
-                break;
-             default:
-                 System.out.printf("not foun");
-                 //dead
-                break;
-        }
         Ingredient a = new Ingredient("Spaghetti",50);
         Ingredient b = new Ingredient("Onion",50);
         Ingredient c = new Ingredient("Water",50);
@@ -61,5 +37,29 @@ public class Main {
 
     private void menu() {
         //SHOW MENU -- ADD OPTIONS
+        int main_menu_op=0;
+
+        System.out.print("Main Menu\n\n 1. Order\n 2. Product Management\n 3. Inventory Management\n\nChoose your option: ");
+        Scanner mmo = new Scanner(System.in);
+        main_menu_op = mmo.nextInt();
+        switch(main_menu_op){
+            case 1:
+                System.out.printf("1");
+                //should display products from products class and need to create another switch here
+                //order
+                break;
+            case 2:
+                System.out.printf("2");
+                //product_management
+                break;
+            case 3:
+                System.out.printf("3");
+                //inventory_management
+                break;
+            default:
+                System.out.printf("not foun");
+                //dead
+                break;
+        }
     }
 }
