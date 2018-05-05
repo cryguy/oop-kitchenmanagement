15422c2065437b5f92998b878dd5a35186e14c3a
cryguy <cry@cry.zone>
2018-05-05 17:24:27 +0800

    edit todo's
    Signed-off-by: cryguy <cry@cry.zone>

--- src/com/shy/OrderManagement.java
+++ src/com/shy/OrderManagement.java
@@ -9,46 +9,40 @@ class OrderManagement {
 
     private OrderManagement() {
     }
-        /*
-        TODO: Save order, delete order, show price, select order
-         */
-        synchronized static OrderManagement getInstance() {
-            if(instance == null) {
-                instance = new OrderManagement();
-            }
-            return instance;
-        }
 
-    void NewOrderX() {
+    synchronized static OrderManagement getInstance() {
+        if (instance == null) {
+            instance = new OrderManagement();
+        }
+        return instance;
+    }
+
+    Order NewOrder() {
         Order i = new Order(counter++);
         orders.add(i);
+        return i;
         // counter works better than size, because we will
         // need to check for a lot of other stuff if we use size()+1
     }
-        Order NewOrder () {
-            Order i = new Order(counter++);
-            orders.add(i);
-            return i;
-            // counter works better than size, because we will
-            // need to check for a lot of other stuff if we use size()+1
-        }
-        Order GetOrder (int i) {
-            for (int x = 0; x <= this.orders.size(); x++) {
-                if (this.orders.get(x).getName() == i) {
-                    return this.orders.get(x);
-                }
+
+    Order GetOrder(int i) {
+        for (int x = 0; x <= this.orders.size(); x++) {
+            if (this.orders.get(x).getName() == i) {
+                return this.orders.get(x);
             }
-            return NewOrder(); // wont ever happen but who knows...
         }
-        void SaveOrder (Order i){
-            orders.add(i);
-        }
+        return NewOrder(); // wont ever happen but who knows...
+    }
 
-        void DeleteOrder (Order i){
-            orders.remove(i);
-        }
+    void SaveOrder(Order i) {
+        orders.add(i);
+    }
 
-        ArrayList<Order> ShowOrders (){
-            return this.orders;
-        }
+    void DeleteOrder(Order i) {
+        orders.remove(i);
+    }
+
+    ArrayList<Order> ShowOrders() {
+        return this.orders;
+    }
 }
