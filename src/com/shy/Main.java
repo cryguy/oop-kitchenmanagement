0560fb2bb11a0f6c54459a425647c0401e380362
cryguy <cry@cry.zone>
2018-05-07 13:54:56 +0800

    do stuff

--- src/com/shy/Main.java
+++ src/com/shy/Main.java
@@ -7,11 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.mainmenu();
-    }
 
-    public static int max(int num1, int num2) {
-        if (num1 < num2)
-            return num1;
-        else return num2;
     }
 }
