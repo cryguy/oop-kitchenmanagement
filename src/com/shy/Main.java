4e51b8db2df5d9a88bc95ec90efc0a5938c481fe
cryguy <cry@cry.zone>
2018-05-08 13:47:35 +0800

    Revert "do stuff"
    This reverts commit 0560fb2bb11a0f6c54459a425647c0401e380362.

--- src/com/shy/Main.java
+++ src/com/shy/Main.java
@@ -7,6 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.mainmenu();
+    }
 
+    public static int max(int num1, int num2) {
+        if (num1 < num2)
+            return num1;
+        else return num2;
     }
 }
